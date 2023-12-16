ac3558b7c7140608347b7c6b89038a31a2e49cf6
15jkw205 <brawlgodbs@gmail.com>
2023-12-16 00:03:21 -0700

    CRUD now in GitHub

---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,52 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        Iterable<Book> booksIterable = bookRepository.findAll();
+        List<Book> booksList = new ArrayList<>();
+        booksIterable.forEach(booksList::add);
+        return booksList;
+    }
+
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Long id, Book updatedBook) {
+        Optional<Book> existingBook = bookRepository.findById(id);
+        if (existingBook.isPresent()) {
+            Book book = existingBook.get();
+            book.setTitle(updatedBook.getTitle());
+            book.setAuthor(updatedBook.getAuthor());
+            book.setYear(updatedBook.getYear());
+            return bookRepository.save(book);
+        }
+        return null;
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
